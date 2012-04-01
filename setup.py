2b7fc2c8fef3bfacdc26104763053ba47872e4f6
Daniel Richman <main@danielrichman.co.uk>
2012-04-01 17:16:39 +0100

    Fix dependencies

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 setup(
     name="couch-named-python",
-    version="0.1.4dev",
+    version="0.1.6dev",
     author="Daniel Richman",
     author_email="main@danielrichman.co.uk",
     url="https://github.com/danielrichman/couch-named-python",
     description="CouchDB view server that executes functions "
                 "on the python path by name",
     packages=["couch_named_python"],
-    requires=["yaml", "couchdbkit"],
+    install_requires=["PyYAML", "couchdbkit"],
     license="GNU General Public License Version 3",
     scripts=["bin/couch-named-python", "bin/cnp-upload"]
 )
